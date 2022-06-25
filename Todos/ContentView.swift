a15f1202e376c96f75b706742c70817c6e05eea5
YJ Soon <yjsoon@tinkertanker.com>
2022-06-25 11:33:53 +0800

    Added bindings

--- Todos/ContentView.swift
+++ Todos/ContentView.swift
@@ -17,16 +17,20 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            List(todos) { todo in
-                HStack {
-                    Image(systemName: todo.isCompleted ? "checkmark.circle.fill" : "circle")
-                    VStack(alignment: .leading) {
-                        Text(todo.title)
-                            .strikethrough(todo.isCompleted)
-                        if !todo.details.isEmpty {
-                            Text(todo.details)
-                                .font(.caption)
-                                .foregroundColor(.gray)
+            List($todos) { $todo in
+                NavigationLink {
+                    TodoDetailView(todo: $todo)
+                } label: {
+                    HStack {
+                        Image(systemName: todo.isCompleted ? "checkmark.circle.fill" : "circle")
+                        VStack(alignment: .leading) {
+                            Text(todo.title)
+                                .strikethrough(todo.isCompleted)
+                            if !todo.details.isEmpty {
+                                Text(todo.details)
+                                    .font(.caption)
+                                    .foregroundColor(.gray)
+                            }
                         }
                     }
                 }
--- Todos/TodoDetailView.swift
+++ Todos/TodoDetailView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct TodoDetailView: View {
     
-    @State var todo = Todo(title: "Water the cat")
+    @Binding var todo: Todo
     
     var body: some View {
         VStack {
@@ -32,6 +32,6 @@ struct TodoDetailView: View {
 
 struct TodoDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        TodoDetailView()
+        TodoDetailView(todo: .constant(Todo(title: "Water the cat demo")))
     }
 }
