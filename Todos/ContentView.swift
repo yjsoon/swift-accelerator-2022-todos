05cd5e766e2d0e9cde7162f93a30571fd088bb52
YJ Soon <yjsoon@tinkertanker.com>
2022-06-25 10:37:37 +0800

    Added NavigationView, @State for todos, and title

--- Todos/ContentView.swift
+++ Todos/ContentView.swift
@@ -9,15 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var todos = [
+    @State var todos = [
         Todo(title: "Watch some Paw Patrol"),
         Todo(title: "Conduct a giveaway"),
         Todo(title: "Randomly deduct some points")
     ]
     
     var body: some View {
-        List(todos) { todo in
-            Text(todo.title)
+        NavigationView {
+            List(todos) { todo in
+                Text(todo.title)
+            }
+            .navigationTitle("Todos")
         }
     }
 }
